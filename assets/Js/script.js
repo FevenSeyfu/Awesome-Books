4fe7135f9a4531aeeda24f957a1380556237a5c0
abahuugbede_dev <89143961+EngruugbeJimmy@users.noreply.github.com>
2023-01-17 21:31:15 +0100

    add local storage

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,23 +1,45 @@
-const addBookBtn = document.getElementById('add-btn');
 const removeBookBtns = document.querySelectorAll('.remove-btn');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const listContainer = document.getElementById('display-book-list');
-const books = [
-  {
-    title: 'book one',
-    author: 'daniel mistry',
-  },
-  {
-    title: 'book two',
-    author: 'book mistry',
-  },
-  {
-    title: 'book three',
-    author: 'author third',
-  },
-];
-
+const addbookForm = document.getElementById('add-form');
+const bookStore = [];
+​
+// Add books and store in localstorage
+function addBooks() {
+  const Book = {
+    title: title.value,
+    author: author.value,
+  };
+  bookStore.push(Book);
+  const jsonData = JSON.stringify(bookStore);
+  window.localStorage.setItem('books', jsonData);
+}
+//  add book button event listener
+addbookForm.addEventListener('submit', () => {
+  addBooks();
+});
+​
+// display books collection
+function fetchData() {
+  const BooksList = localStorage.getItem('books');
+  const parseBooksList = JSON.parse(BooksList);
+  parseBooksList.forEach((book) => {
+    const bookList = document.createElement('ul');
+    bookList.classList.add('book-list');
+    listContainer.append(bookList);
+    bookList.innerHTML = `<li class="book-title">${book.title}</li>
+        <li class="book-author">${book.author}</li>
+        <button class="remove-btn" type="reset">Remove</button>
+        <hr>
+      <li>`;
+  });
+}
+​
+window.onload = () => {
+  fetchData();
+};
+​
 // remove book from list(author,title)
 function removeBooks(index) {
   books.splice(index, 1);
@@ -30,36 +52,3 @@ removeBookBtns.forEach((removeBookBtn) => {
     removeBooks(id);
   });
 });
-
-// display books collection
-function displayBooksList() {
-  // const books = Object.values('books');
-  books.forEach((book) => {
-    const bookList = document.createElement('ul');
-    bookList.classList.add('book-list');
-    listContainer.append(bookList);
-    bookList.innerHTML = `<li class="book-title">${book.title}</li>
-        <li class="book-author">${book.author}</li>
-        <button class="remove-btn" type="reset">Remove</button>
-        <hr>
-      <li>`;
-  });
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  displayBooksList();
-});
-
-// Add book to list ( author, title)
-function addBooks(e) {
-  // console.log('add');
-  e.preventDefault();
-  const newBook = {
-    title: title.value,
-    author: author.value,
-  };
-  books.push(newBook);
-  window.location.reload();
-}
-//  add book button event listener
-addBookBtn.addEventListener('click', addBooks);
