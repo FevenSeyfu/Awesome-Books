51d8c6ba5bee29f12a4499243a44c3bcf68d270c
FevenSeyfu <fevenseyfu00@gmail.com>
2023-01-17 21:00:44 +0300

    Add diplay book list

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -2,7 +2,7 @@ const addBookBtn = document.getElementById('add-btn');
 const removeBookBtns = document.querySelectorAll('.remove-btn');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-
+const listContainer = document.getElementById('display-book-list');
 const books = [
   {
     title: 'book one',
@@ -18,20 +18,6 @@ const books = [
   },
 ];
 
-// Add book to list ( author, title)
-function addBooks(e) {
-  // console.log('add');
-  e.preventDefault();
-  const newBook = {
-    title: title.value,
-    author: author.value,
-  };
-  books.push(newBook);
-}
-
-//  add book button event listener
-addBookBtn.addEventListener('click', addBooks);
-
 // remove book from list(author,title)
 function removeBooks(index) {
   books.splice(index, 1);
@@ -44,3 +30,36 @@ removeBookBtns.forEach((removeBookBtn) => {
     removeBooks(id);
   });
 });
+
+// display books collection
+function displayBooksList() {
+  // const books = Object.values('books');
+  books.forEach((book) => {
+    const bookList = document.createElement('ul');
+    bookList.classList.add('book-list');
+    listContainer.append(bookList);
+    bookList.innerHTML = `<li class="book-title">${book.title}</li>
+        <li class="book-author">${book.author}</li>
+        <button class="remove-btn" type="reset">Remove</button>
+        <hr>
+      <li>`;
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayBooksList();
+});
+
+// Add book to list ( author, title)
+function addBooks(e) {
+  // console.log('add');
+  e.preventDefault();
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  books.push(newBook);
+  window.location.reload();
+}
+//  add book button event listener
+addBookBtn.addEventListener('click', addBooks);
